bbd37040cc8817eb527eb4011813c0f2b0afc339
sreeprasad <sreeprasad.sp@gmail.com>
2014-12-21 14:26:04 -0500

    adding number class that extends symbol but accepts numebr

---
+++ Number.java
@@ -0,0 +1,26 @@
+public class Number extends Symbol{
+
+	public  int item;
+	
+	public Symbol(String input){
+		super(null,null,input);
+		item = Integer.parseInt(item);
+	}
+
+	public Symbol(int input){
+		super(null,null,input);
+		this.item=input;
+	}
+
+	public int precedence(){
+		return precedence;
+	}
+
+	@override
+	Node build(){
+		return new LeafNode(item);
+	}
+
+
+
+}
