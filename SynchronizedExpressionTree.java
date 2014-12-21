308c32925c6442b9d3aaa622173613027d7c452a
sreeprasad <sreeprasad.sp@gmail.com>
2014-12-20 23:16:59 -0500

    synchornized expression tree makes Expression tree methods thread safe

---
+++ SynchronizedExpressionTree.java
@@ -0,0 +1,57 @@
+import java.util.Iterator;
+public class SynchronizedExpressionTree extends ExpressionTree {
+
+ 
+	private TreeIteratorFactory iteratorFactory = new TreeIteratorFactory();
+
+	public SynchronizedExpressionTree (Node root){
+		super(root);
+	}
+
+	public Iterator<ExpressionTree> getIterator(String traversal){
+		Iterator<ExpressionTree> synchIterator=null;
+		synchronized(this){
+				synchIterator=iteratorFactory.makeIterator(this,traversal);
+		}
+		return synchIterator;
+	}
+
+	public boolean isNull(){
+		boolean isSuperRootNull = false;
+		synchronized(this){
+			isSuperRootNull=super.isNull();
+		}
+		return isSuperRootNull;
+	}
+
+	public ExpressionTree getLeft(){
+		ExpressionTree superLeft=null;
+		synchronized(this){
+			superLeft= super.getLeft();
+		}
+		return superLeft;
+	}
+
+	public ExpressionTree getRight(){
+		ExpressionTree superRight=null;
+		synchronized(this){
+			superRight= super.getRight();
+		}
+		return superRight;
+	}
+
+	public void accept(Visitor visitor){
+		synchronized(this){
+			super.accept(visitor);	
+		}
+	}
+
+	public int item(){
+		int item;
+		synchronized(this){
+			item=super.item();
+		}
+		return item;
+	}
+
+}
