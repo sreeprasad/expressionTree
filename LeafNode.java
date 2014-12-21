0e835f2f8ad2d6868e3e1d1f38334eaabc1a1121
sreeprasad <sreeprasad.sp@gmail.com>
2014-12-20 20:49:16 -0500

    added accept method and one more constrctor

--- LeafNode.java
+++ LeafNode.java
@@ -11,4 +11,12 @@ public class LeafNode extends Node{
 		return this.item;
 	}
 
+	public void accept(Visitor visitor){
+		visitor.visit(this);
+	}
+
+	public LeafNode(String item){
+		this.item=Integer.parseInt(item);
+	}
+
 }
